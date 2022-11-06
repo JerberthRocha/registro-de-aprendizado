806eb525cf5a91477f198382180706f0fff8d9ba
JerberthRocha <jerberthrs@gmail.com>
2022-11-06 17:35:15 -0400

    Criando template para edit_entry e editando algumas entradas

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.index, name="index"), # Página inicial
     path('topics/', views.topics, name='topics'), # Mostra todos os assuntos
     path('new_topic/', views.new_topic, name='new_topic'), # # Página para adicionar um novo assunto
-    path('topics/<int:id>/', views.topic, name='topic'), # Página de detalhes para um único assunto
-    path('new_entry/<int:id>/', views.new_entry, name='new_entry'), # Página para adicionar uma nova entrada
-    path('edit_entry/<int:id>/', views.edit_entry, name='edit_entry'), # # Página para editar uma entrada
+    path('topics/<int:topic_id>/', views.topic, name='topic'), # Página de detalhes para um único assunto
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'), # Página para adicionar uma nova entrada
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'), # # Página para editar uma entrada
 ]
