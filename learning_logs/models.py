48574b364cb3fce04ae34d8c89c0bbbc56fe0d61
JerberthRocha <jerberthrs@gmail.com>
2022-11-07 15:22:08 -0400

    Migrando o banco de dados e associando tópicos ao usuário

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,7 @@ class Topic(models.Model):
     """Um assunto sobre o qual o usuário está aprendendo."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
         """Devolve uma representação em string do model."""
