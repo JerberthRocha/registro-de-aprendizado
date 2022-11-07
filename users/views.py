af7cb0585751246fbbdf3c9dfc4c5f9033c5308b
JerberthRocha <jerberthrs@gmail.com>
2022-11-06 21:13:00 -0400

    Criando função de view register()

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -9,3 +10,23 @@ def logout_view(request):
     """Faz logout do usuário."""
     logout(request)
     return (HttpResponseRedirect(reverse('learning_logs:index')))
+
+def register(request):
+    """Faz o cadastro de um novo usuário."""
+    if request.method != 'POST':
+        # Exibe o formulário de cadastro em branco
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Faz login do usuário e o redireciona para a página inicial
+            authenticate_user = authenticate(
+                username=new_user.username,
+                password=request.POST['password1']
+            )
+            login(request, authenticate_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    
+    context = {'form': form}
+    return render(request, 'users/pages/register.html', context)
