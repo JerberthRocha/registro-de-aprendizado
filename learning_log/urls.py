7ebcf2f6e31362b07f42474fc6790f8247ace753
JerberthRocha <jerberthrs@gmail.com>
2022-11-08 20:43:46 -0400

    Refatorando urls.py de  learning_log

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from learning_logs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
